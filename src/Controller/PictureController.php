1af38b1dcf47295cc2f704076202bc103f5bc261
Sébastien Blache <sbblache@gmail.com>
2021-10-16 20:00:17 +0200

    basic controller Picture

---
+++ src/Controller/PictureController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArtistRepository;
+use App\Repository\CategoryRepository;
+use App\Repository\PictureRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PictureController extends AbstractController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(PictureRepository $pictureRepository, CategoryRepository $categoryRepository, ArtistRepository $artistRepository): Response
+    {
+        $category = $categoryRepository->findAll();
+
+        $pictures = $pictureRepository->findAll();
+
+        $artist = $artistRepository->findAll();
+
+        return $this->render('admin/index.html.twig', [
+            'pictures' => $pictures,
+            'categories' => $category,
+            'artists' => $artist
+
+        ]);
+    }
+
+
+}
